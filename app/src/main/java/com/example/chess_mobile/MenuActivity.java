d9b1bd84f3fc94b826f8ae7ec6dccac61929a7ae
joaquigb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2022-05-16 14:29:39 +0200

    Creando sistema de promoción del peón

--- app/src/main/java/com/example/chess_mobile/MenuActivity.java
+++ app/src/main/java/com/example/chess_mobile/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.example.chess_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_actinity);
+
+        BottomNavigationView bottomNav = findViewById(R.id.main_menu);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_Play:
+                    selectedFragment = new PlayFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, selectedFragment).commit();
+
+            return true;
+        });
+
     }
 }
